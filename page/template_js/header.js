8a697f199fde2c51b1024c7aa129cfc5ce5030e5
CHiến <cuutrang0106@gmail.com>
2022-09-06 15:39:21 +0700

    up

--- page/template_js/header.js
+++ page/template_js/header.js
@@ -3,19 +3,17 @@ function createHeader() {
     return ` <header class="header">
     <div class="header-top">
         <div class="logo">
-            <img src="../img/image 3.png"
-            
-            " alt="">
+            <img src="../img/image 3.png" alt="" >
         </div>
         <div class="login-register">
             <i class="fa-solid fa-cart-shopping">(1)</i>
             <a href="#">Login</a>
-            <a href="http://127.0.0.1:5501/page/register.html">Register</a>
+            <a href="http://127.0.0.1:5500/Capstone2nop/page/register.html">Register</a>
         </div>
         </div>
         <div class="header-bottom">
             <nav>
-                <a class="home" href="http://127.0.0.1:5501/index.html">Home</a>
+                <a class="home" href="#">Home</a>
                 <a href="#">Men</a>
                 <a href="#">Woman</a>
                 <a href="#">Kid</a>
